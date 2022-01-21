e3e178a0b3fb4423748881cb69e6c2aaf8cb8569
AntoineMattei <pistoine@gmail.com>
2022-01-21 15:12:01 +0100

    Renommage des fichiers d'activitÃ©

--- app/src/main/java/esimed/amattei/examen/rien/RechercheParDepartementActivity.kt
+++ app/src/main/java/esimed/amattei/examen/rien/RechercheParDepartementActivity.kt
@@ -3,9 +3,9 @@ package esimed.amattei.examen.rien
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RechercheParDepartementActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recherche_par_departement)
     }
 }
